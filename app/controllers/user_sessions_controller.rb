26045cab81306aad11e480802fb4384fdd739463
neofluxs <neofluxs@me.com>
2014-09-04 00:09:55 +0000

    finished first part of course

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,15 @@
+class UserSessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])	
+       session[:user_id] = user.id
+       flash[:sucess] = "Signed in successfully"
+    else
+       render action: 'new'
+       flash[:error] = "Login Unsucessful, please check your email and/or password"
+    end
+    end
+end
