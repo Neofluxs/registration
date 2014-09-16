b575f5468d062d32521ef51c6b12733e643bc86a
neofluxs <neofluxs@me.com>
2014-09-16 18:55:17 +0000

    layed all basic foundations, design needed, some links are broken, some views are unaccessible except through URL

--- app/controllers/event_entries_controller.rb
+++ app/controllers/event_entries_controller.rb
@@ -1,13 +1,13 @@
 class EventEntriesController < ApplicationController
   before_action :require_user
   def index
-    @evententry = EventEntry.all
+    @evententry = current_user.event_entries.all
   end
   def new
     @evententry = EventEntry.new
   end
   def create
-    @evententry = current_user.evententry.new(evententry_params)
+    @evententry = current_user.event_entries.new(evententry_params)
       if @evententry.save
         redirect_to events_path, notice: 'Registered successfully'
       else
@@ -18,6 +18,6 @@ class EventEntriesController < ApplicationController
     @evententry = EventEntry.find(params[:id])
   end
   def evententry_params
-    params.require(:evententry).permit(:first_name, :middle_name, :last_name, :name_on_certificate, :accomodation, :paid)
+    params.require(:event_entry).permit(:first_name, :middle_name, :last_name, :name_on_certificate, :accomodation, :paid)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,3 @@
 class Event < ActiveRecord::Base
-  has_many :event_entry
+  has_many :event_entries
 end
